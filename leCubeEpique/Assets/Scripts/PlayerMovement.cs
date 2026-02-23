5b356578059c246350fed371a48d40d6c50e99fd
SirChickenBro <freddiewashington5555@gmail.com>
2026-02-23 13:55:33 -0700

    Add rigidbody + basic player script

---
+++ leCubeEpique/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
