d29f6e9acef3eb9db4fbdbb78b2c0fdbeaf0fd33
SirChickenBro <freddiewashington5555@gmail.com>
2026-02-25 13:49:03 -0700

    make camera follow player

--- leCubeEpique/Assets/Scripts/PlayerMovement.cs
+++ leCubeEpique/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 1000f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
